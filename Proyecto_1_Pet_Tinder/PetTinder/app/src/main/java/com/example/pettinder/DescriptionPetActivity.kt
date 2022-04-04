0a04a309330f77c5a76b98638195b5b3bf1f37b7
eduardoportillo <52632798+eduardoportillo@users.noreply.github.com>
2022-04-04 00:41:14 -0400

    .

--- Proyecto_1_Pet_Tinder/PetTinder/app/src/main/java/com/example/pettinder/DescriptionPetActivity.kt
+++ Proyecto_1_Pet_Tinder/PetTinder/app/src/main/java/com/example/pettinder/DescriptionPetActivity.kt
@@ -1,11 +1,32 @@
 package com.example.pettinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DescriptionPetActivity : AppCompatActivity() {
+
+    lateinit var btnVolverMain: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_description_pet)
+
+        btnVolverMain = findViewById(R.id.btnVolverMain)
+
+        setupEventListener()
+
+    }
+
+    private fun setupEventListener() {
+        btnVolverMain.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+
+            //para enviar datos de Activity 1 al 2
+            //intent.putExtra("varName", nombre)
+
+            startActivity(intent)
+        }
     }
 }
