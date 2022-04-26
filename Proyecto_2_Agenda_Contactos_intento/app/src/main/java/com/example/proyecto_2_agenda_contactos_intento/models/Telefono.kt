824228b163c8af7febee81def21e734f3dc1e78c
eduardoportillo <52632798+eduardoportillo@users.noreply.github.com>
2022-04-26 05:44:02 -0400

    proyecto 2

---
+++ Proyecto_2_Agenda_Contactos_intento/app/src/main/java/com/example/proyecto_2_agenda_contactos_intento/models/Telefono.kt
@@ -0,0 +1,10 @@
+package com.example.practicanavegacion.ui.models
+
+import java.io.Serializable
+
+class Telefono (
+    var id: Int,
+    var numero: Int,
+    var id_contacto: Int,
+    var tipo_telefono: String
+): Serializable
