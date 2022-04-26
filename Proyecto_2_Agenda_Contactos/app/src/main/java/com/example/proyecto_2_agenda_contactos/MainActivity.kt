ebeaffb467af4065765fb83249d5f869317a9cdd
eduardoportillo <52632798+eduardoportillo@users.noreply.github.com>
2022-04-26 19:53:36 -0400

    ETLCPAS

--- Proyecto_2_Agenda_Contactos/app/src/main/java/com/example/proyecto_2_agenda_contactos/MainActivity.kt
+++ Proyecto_2_Agenda_Contactos/app/src/main/java/com/example/proyecto_2_agenda_contactos/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.contactoFragment,R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.contactoFragment,R.id.formContactFragment
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
