0b90cb2360ca0b230a5e300c68c4058ea567a066
eduardoportillo <52632798+eduardoportillo@users.noreply.github.com>
2022-04-26 23:29:32 -0400

    creando contacto

--- Proyecto_2_Agenda_Contactos/app/src/main/java/com/example/proyecto_2_agenda_contactos/ui/fragments/contacto/FormContactFragment.kt
+++ Proyecto_2_Agenda_Contactos/app/src/main/java/com/example/proyecto_2_agenda_contactos/ui/fragments/contacto/FormContactFragment.kt
@@ -5,56 +5,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.fragment.findNavController
 import com.example.proyecto_2_agenda_contactos.R
+import com.example.proyecto_2_agenda_contactos.datasingleton.ContactoItem
+import com.example.proyecto_2_agenda_contactos.models.Contacto
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FormContactFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FormContactFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var inputName: EditText
+    lateinit var inputApellido: EditText
+    lateinit var inputCiudad: EditText
+    lateinit var inputEdad: EditText
+    lateinit var inputEmail: EditText
+    lateinit var inputDireccion: EditText
+    lateinit var inputTelefono: EditText
+    lateinit var btnGuardar: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_form_contact, container, false)
+        val view = inflater.inflate(R.layout.fragment_form_contact, container, false)
+        inputName = view.findViewById(R.id.inputName)
+        inputApellido = view.findViewById(R.id.inputApellido)
+        inputCiudad = view.findViewById(R.id.inputCiudad)
+        inputEdad = view.findViewById(R.id.inputEdad)
+        inputEmail = view.findViewById(R.id.inputEmail)
+        inputDireccion = view.findViewById(R.id.inputDireccion)
+        inputTelefono = view.findViewById(R.id.inputTelefono)
+        btnGuardar = view.findViewById(R.id.btnGuardar)
+
+        btnGuardar.setOnClickListener {
+            ContactoItem.createContact(
+                inputName.text.toString(),
+                inputApellido.text.toString(),
+                "\\",
+                inputCiudad.text.toString(),
+                inputEdad.text.toString().toInt(),
+                inputEmail.text.toString(),
+                inputDireccion.text.toString(),
+                inputTelefono.text.toString().toInt()
+            ) //TODO canviar url
+            
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FormContactFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FormContactFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
