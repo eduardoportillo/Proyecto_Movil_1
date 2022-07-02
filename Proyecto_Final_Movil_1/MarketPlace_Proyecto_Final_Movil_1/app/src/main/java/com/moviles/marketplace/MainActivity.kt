908036b4ec2b68cafc482d7aefdb8d6cd58517e6
eduardoportillo <eportillovirreira@gmail.com>
2022-07-02 14:46:39 -0400

    implementando configuración de firebase

--- Proyecto_Final_Movil_1/MarketPlace_Proyecto_Final_Movil_1/app/src/main/java/com/moviles/marketplace/MainActivity.kt
+++ Proyecto_Final_Movil_1/MarketPlace_Proyecto_Final_Movil_1/app/src/main/java/com/moviles/marketplace/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.moviles.marketplace
 
+import android.content.ContentValues
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.FirebaseApp
+import com.google.firebase.messaging.FirebaseMessaging
 import com.moviles.marketplace.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -31,5 +36,23 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        setupFirebaseToken()
+    }
+
+    private fun setupFirebaseToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener{ task ->
+            if (!task.isSuccessful) {
+                Log.w(ContentValues.TAG, "Error al obtener el token de registro de FCM", task.exception)
+                return@addOnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            Log.d("TOKEN_FIREBASE", token)
+//            Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+        }
     }
 }
