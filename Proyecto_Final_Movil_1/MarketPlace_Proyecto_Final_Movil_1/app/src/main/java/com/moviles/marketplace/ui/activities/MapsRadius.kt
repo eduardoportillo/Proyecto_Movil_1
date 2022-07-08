7d9c44fa15555078f67439a6b5123137808d64bf
eduardoportillo <eportillovirreira@gmail.com>
2022-07-07 21:17:09 -0400

    mapa radius ready

--- Proyecto_Final_Movil_1/MarketPlace_Proyecto_Final_Movil_1/app/src/main/java/com/moviles/marketplace/ui/activities/MapsRadius.kt
+++ Proyecto_Final_Movil_1/MarketPlace_Proyecto_Final_Movil_1/app/src/main/java/com/moviles/marketplace/ui/activities/MapsRadius.kt
@@ -2,13 +2,13 @@ package com.moviles.marketplace.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
 import com.google.android.gms.maps.CameraUpdateFactory
+
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.Circle
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.moviles.marketplace.R
 import com.moviles.marketplace.databinding.ActivityMapsRadiusBinding
 
@@ -16,6 +16,10 @@ class MapsRadius : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsRadiusBinding
+    private var globalMarker: LatLng = LatLng(-17.8145819, -63.1560853)
+    private var globalRadius: Double = 1000.0
+    private var zoom: Float = 5000.0f
+    private var circle: Circle? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +27,6 @@ class MapsRadius : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsRadiusBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -31,10 +34,29 @@ class MapsRadius : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(globalMarker, zoom))
+        mMap.setOnCameraMoveListener {
+            globalMarker = mMap.cameraPosition.target
+            drawCircle(globalMarker)
+        }
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+    }
+
+    private fun setupMyLocation() {
+        TODO("Not yet implemented")
+    }
+
+    private fun drawCircle(point: LatLng) {
+        if (this.circle != null) {
+            this.circle?.remove()
+        }
+        this.circle = mMap.addCircle(
+            com.google.android.gms.maps.model.CircleOptions()
+                .center(point)
+                .radius(globalRadius)
+                .strokeColor(0x00ff0000)
+                .fillColor(0x30ff0000)
+        )
     }
 }
